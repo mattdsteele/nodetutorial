779483a820bff5596b3b39101c392899aeac52bb
Matt Steele <orphum@gmail.com>
2011-07-19 21:26:15 -0500

    In tutorial - through routing section

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route) {
   onRequest = function(request, response) {
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write("Hello, world!");
-      response.end();
-      };
+    var path = url.parse(request.url).pathname;
+    console.log("request received for " + path);
+
+    route(path);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello, world!");
+    response.end();
+  };
   http.createServer(onRequest).listen(8888);
   console.log("Job has started.");
 }
