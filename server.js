8591085a63d42d83302c00426810c0f64f4540d0
Matt Steele <orphum@gmail.com>
2011-07-19 21:15:51 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+function start() {
+  onRequest = function(request, response) {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write("Hello, world!");
+      response.end();
+      };
+  http.createServer(onRequest).listen(8888);
+  console.log("Job has started.");
+}
+
+exports.start = start;
