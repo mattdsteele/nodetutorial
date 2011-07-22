97303745837683b242643adf8f8fa9aae0569e8b
Matt Steele <orphum@gmail.com>
2011-07-21 21:58:39 -0500

    finished with "handling post requests" section

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var url = require('url');
 
 function start(route, handle) {
   onRequest = function(request, response) {
+    var postData = "";
     var path = url.parse(request.url).pathname;
     console.log("request received for " + path);
 
-    route(handle, path, response);
+    request.setEncoding("utf-8");
+    request.addListener("data", function(chunk) {
+        postData += chunk;
+    });
+    request.addListener("end", function() {
+        route(handle, path, response, postData);
+    });
   };
 
   http.createServer(onRequest).listen(8888);
