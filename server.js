b6bae259c3e1c96de10995aa2df5b8534ab6ef9a
Matt Steele <orphum@gmail.com>
2011-07-25 14:22:39 -0500

    finished tutorial

--- server.js
+++ server.js
@@ -3,17 +3,9 @@ var url = require('url');
 
 function start(route, handle) {
   onRequest = function(request, response) {
-    var postData = "";
     var path = url.parse(request.url).pathname;
     console.log("request received for " + path);
-
-    request.setEncoding("utf-8");
-    request.addListener("data", function(chunk) {
-        postData += chunk;
-    });
-    request.addListener("end", function() {
-        route(handle, path, response, postData);
-    });
+    route(handle, path, request, response);
   };
 
   http.createServer(onRequest).listen(8888);
